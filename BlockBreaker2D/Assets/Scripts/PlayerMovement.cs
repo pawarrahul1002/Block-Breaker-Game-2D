4e0e0d485d58e738a9625cda2820682dc5058b8d
pawarrahul1002 <pawar.rahul1002@gmail.com>
2021-07-30 19:22:36 +0530

    Added player Movement

---
+++ BlockBreaker2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Player 1");
+        
+    }
+
+}
